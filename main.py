4944ac3174c7516a36276210b083a7d0f5af6683
Lanxtot <80767096+Lanxtot@users.noreply.github.com>
2024-10-30 15:35:42 +0000

    v1

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-"""
-# Prekybos robotas (simple)
+l1=[
+    'Volvo 7700A (2004) Nr. 701',
+    'Volvo 7700A (2004) Nr. 702',
+    'Volvo 7700A (2004) Nr. 703',
+    'Volvo 7700A (2004) Nr. 706',
+    'Volvo 7700A (2004) Nr. 708',
+    'Volvo 7700A (2004) Nr. 709',
+]
 
-### Užduotis
+l2=[
+    3499.99,
+    2699.99,
+    1749.99,
+    2129.99,
+    1409.49,
+    5799.99
 
-Parašykite programą, kuri imituotų prekybos automatą. Programa turėtų paprašyti vartotojo įvesti pinigų sumą, kurią jis nori išleisti, ir tada parodyti prekių, kurias jis gali įsigyti pagal įvestą pinigų sumą, sąrašą.
+]
 
-1. Prekybos automate yra šios prekės ir kainos:
-    - Saldainiai: 0,50€
-    - Traškučiai: 1,00€
-    - Gėrimas: 1,50€
-    - Kramtomoji guma: 0,75€
-2. Programa turėtų paprašyti vartotojo įvesti pinigų sumą.
-    - Jei vartotojas įveda mažesnę nei 0,50€ sumą, rodomas klaidos pranešimas: "Jūsų įvestos sumos nepakanka jokiems daiktams įsigyti."
-    - Jei vartotojas įveda sumą, didesnę ar lygią 0,50€, programa turėtų parodyti prekes, kurias jis gali įsigyti pagal įvestą sumą:
-        - Jei suma mažesnė nei 0,75€, vartotojas gali rinktis tik iš "Saldainiai".
-        - Jei suma yra tarp 0,75€ ir 1,00€, vartotojas gali rinktis iš "Saldainiai" arba "Kramtomoji guma".
-        - Jei suma yra tarp 1,00€ ir 1,50€, vartotojas gali rinktis iš "Saldainiai", "Traškučiai" arba "Kramtomoji guma".
-        - Jei suma yra didesnė nei 1,50€, rodomas visas prekių sąrašas.
-3. Programa turi paprašyti vartotojo pasirinkti prekę ir parodyti likutį po pirkimo. Jei likutis yra mažesnis nei 0,50€, rodomas pranešimas: "Dėkojame už Jūsų pirkinį. Jūsų likęs likutis yra [likutis]."
+l=[]
 
-### Pastabos
+p=float(input("Įveskite sumą, kurią norėtumėte išleisti: €"))
 
-- Naudokite funkciją `input()`, kad paprašytumėte vartotojo įvesti duomenis.
-- Naudokite `if...else` struktūras, kad nuspręstumėte, kokius veiksmus atlikti pagal vartotojo įvestą pinigų sumą.
-
-### Veikimo pavyzdys:
-
-```
-Įveskite sumą, kurią norėtumėte išleisti: €0.60
-Galite įsigyti:
-1. Saldainiai - €0.50
-Įveskite pasirinkimo numerį: 1
-Pasirinkote Saldainiai. Jūsų likutis yra €0.10.
-Dėkojame už Jūsų pirkinį. Jūsų likutis yra €0.10.
-```
-
-"""
-
-
-
-
+while True:
+    for i in range(len(l2)):  
+        if l2[i]>p:
+            l1.insert(i,0)
+            l1.pop(i+1)
+            l2.insert(i,0)
+            l2.pop(i+1)
+    l1=[i for i in l1 if i != 0]
+    l2=[i for i in l2 if i != 0]
+    if l2==[]:
+        print(f'Dėkojame už Jūsų pirkinį. Jūsų likutis yra €{p:.2f}.')
+        break
+    else:
+        print('Galite įsigyti:')
+        for i in range(len(l1)):
+            print(f'{str(i+1)}. {l1[i]} - €{str(l2[i])}')
+        r=int(input("Įveskite pasirinkimo numerį: "))
+        p-=l2[r-1]
+        print(f'Pasirinkote {(l1[r-1])}. Jūsų likutis yra €{p:.2f}.')
